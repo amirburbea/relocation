cf9352c5022c77307f677e42e25442e095237c45
Amir Burbea <amir.burbea@gmail.com>
2020-06-18 17:20:56 -0500

    Simplify clear all

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,18 @@
-﻿namespace Relocation
+﻿using System.Linq;
+using System.Windows;
+
+namespace Relocation
 {
     partial class MainWindow
     {
         public MainWindow() => this.InitializeComponent();
+
+        private void ClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (ItemModel item in ((MainViewModel)this.DataContext).Categories.SelectMany(category => category.Items))
+            {
+                item.IsSelected = false;
+            }
+        }
     }
 }
